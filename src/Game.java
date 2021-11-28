6770867acb222b2cb5b3340a0d15751b46cec80d
Luke Mason <lukesamuelmason@gmail.com>
2021-11-28 06:19:56 -0700

    initial push

---
+++ src/Game.java
@@ -0,0 +1,9 @@
+/**
+ * Game.java
+ * 563: HW5 - Game of Life
+ * Created by Luke Mason
+ * Class to represent the game actor for the use-case diagram of the Game of Life
+ */
+
+public class Game {
+}
---
+++ src/Player.java
@@ -0,0 +1,9 @@
+/**
+ * Player.java
+ * 563: HW5 - Game of Life
+ * Created by: Luke Mason
+ * Class to represent the player actor in the use-case diagram for the Game of Life
+ */
+
+public class Player {
+}
