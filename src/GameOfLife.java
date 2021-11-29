0fbd20dd2e05b224cd09e62bd4faeb8ad6903e80
Luke Mason <lukesamuelmason@gmail.com>
2021-11-29 11:56:29 -0700

    finished with generation calculation

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -8,8 +8,11 @@
 public class GameOfLife {
     public static void main(String[] args) {
         // Create game object to run infinite game loop
-        Game curGame = new Game(50);
+        Game curGame = new Game(5);
 
+        curGame.print_generation();
         curGame.run_game();
+        System.out.println();
+        curGame.print_generation();
     }
 }
